9ef4f98eee4743e9a8818fa918e25f497eafd10c
ttpro1995 <ttpro1995@yahoo.com.vn>
2020-01-28 19:44:29 +0700

    routing seem to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,64 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms';
+// import { RouterModule, Routes } from '@angular/router';
+
+// import { AppComponent } from './app.component';
+// import { HelloComponent } from './hello.component';
+// import { SimpleComComponent } from './simple-com/simple-com.component';
+
+// @NgModule({
+//   imports:      [ 
+//     BrowserModule,
+//     FormsModule,
+//     RouterModule.forRoot([
+//       { path: '', component: SimpleComComponent },
+//     ])
+//   ],
+//   declarations: [ AppComponent, HelloComponent, SimpleComComponent ],
+//   bootstrap:    [ AppComponent ]
+// })
+// export class AppModule { }
+
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SimpleComComponent } from './simple-com/simple-com.component';
+import { AbcComponent } from './abc/abc.component';
+
+
+const appRoutes: Routes = [
+      { path: 'sim', component: SimpleComComponent },
+      { path: 'abc', component: AbcComponent}
+];
 
 @NgModule({
-  imports:      [ 
+  imports: [
     BrowserModule,
-    FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: SimpleComComponent },
-    ])
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  declarations: [ AppComponent, HelloComponent, SimpleComComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    SimpleComComponent,
+    AbcComponent
+  ],
+  bootstrap: [ AppComponent],
 })
 export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
